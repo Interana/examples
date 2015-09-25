3275298594962553e3a9f5f1f45a8d1c8c8250a8
jsrawan-mobo <jag@interana.com>
2015-09-25 15:44:39 -0700

    got json server only working

--- jsonserver/app/uwsgi.py
+++ jsonserver/app/uwsgi.py
@@ -1,21 +1,102 @@
+import fnmatch
+import re
 import ujson
+
+
+
+
+
+
+
+# def application(environ, start_response):
+#     status = '200 OK'
+#     """
+#     Sleep for specified time
+#     """
+#     sleep = 0.5
+#     sleep = float(sleep)
+#     gevent.sleep(sleep)
+#     response = {
+#         "message": "CPU Perf test_perf_3 waited for {} ms".format(1000 * sleep)
+#     }
+#     output = ujson.dumps(response)
+#     response_headers = [('Content-type', 'application/json'),
+#                         ('Content-Length', str(len(output)))]
+#
+#     start_response(status, response_headers)
+#     return [output]
 import gevent
 
+def not_found(environ, start_response):
+    start_response('404 Not Found', [('content-type','text/html')])
+    return []
 
-def application(environ, start_response):
-    status = '200 OK'
+def index(environ, start_response):
+    test_perf_1(environ, start_response)
+
+def test_perf_1(environ, start_response):
+    """
+    Basic return response json
+    """
+    response = {
+        "message": "Great Scott, It works!!"
+    }
+    data = ujson.dumps(response)
+    response_headers = [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response('200 OK', response_headers)
+    return data
+
+
+
+def test_perf_3(environ, start_response, sleep=0.05):
     """
     Sleep for specified time
     """
-    sleep = 0.5
     sleep = float(sleep)
     gevent.sleep(sleep)
     response = {
         "message": "CPU Perf test_perf_3 waited for {} ms".format(1000 * sleep)
     }
-    output = ujson.dumps(response)
-    response_headers = [('Content-type', 'application/json'),
-                        ('Content-Length', str(len(output)))]
+    data = ujson.dumps(response)
+    response_headers = [
+        ('Content-type', 'application/json'),
+        ('Content-Length', str(len(data)))
+    ]
 
-    start_response(status, response_headers)
-    return [output]
+    start_response('200 OK', response_headers)
+    return data
+
+
+
+
+## Put routes down here to the message above.  When having parameter, always put all variants in there
+
+urls = [
+    (r'^$', index),
+    (r'test/perf/1/?$', test_perf_1),
+    (r'test/perf/3/(\d.\d+)/?$', test_perf_3),
+    (r'test/perf/3/?$', test_perf_3)
+
+]
+
+def application(environ, start_response):
+    path = environ.get('PATH_INFO', '').lstrip('/')
+    for regex, callback in urls:
+        match = re.search(regex, path)
+        if match is not None:
+            params = match.groups()
+            if len(params) == 0:
+                return callback(environ, start_response)
+            else:
+                return callback(environ, start_response, *params)
+    return not_found(environ, start_response)
+
+
+if __name__ == "__main__":
+
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 9090, application)
+    srv.serve_forever()
