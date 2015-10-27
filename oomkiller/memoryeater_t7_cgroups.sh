dd03743fe83e1f2a16e6d432dde1cef818836126
Ubuntu <ubuntu@ip-10-0-0-180.ec2.internal>
2015-10-27 22:07:22 +0000

    cgroups memory eater

---
+++ oomkiller/memoryeater_t7_cgroups.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+MY_PID=$$
+echo Adding my pid to cgroup : $MY_PID
+echo $MY_PID  | sudo tee /sys/fs/cgroup/memory/interana/cgroup.procs
+for i in $(seq 1 100); do for j in $(seq 1 2); do (./memoryeater_and_kill 10 100 10 50 &) ; done ; sleep 30; done
